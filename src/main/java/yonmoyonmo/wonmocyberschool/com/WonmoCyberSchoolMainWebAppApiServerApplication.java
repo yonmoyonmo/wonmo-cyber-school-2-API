2f0149829563beaa60149c26ef61ecbfc3fc25d9
yonmoyonmo <yonmosound@gmail.com>
2021-02-11 16:58:17 +0900

    start wonmo cyber school 2 api

---
+++ src/main/java/yonmoyonmo/wonmocyberschool/com/WonmoCyberSchoolMainWebAppApiServerApplication.java
@@ -0,0 +1,14 @@
+package yonmoyonmo.wonmocyberschool.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonmoCyberSchoolMainWebAppApiServerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("<<<$$$ [ 원모 싸이버 스쿨 ON ] $$$>>>");
+		SpringApplication.run(WonmoCyberSchoolMainWebAppApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yonmoyonmo/wonmocyberschool/com/WonmoCyberSchoolMainWebAppApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package yonmoyonmo.wonmocyberschool.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonmoCyberSchoolMainWebAppApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
